be6093d850afb5179a83b9f5300cf8c9830b5bad
René van der Ark <rene.vanderark@kb.nl>
2023-09-17 16:19:08 +0200

    adds mock jumping.

--- player/player.gd
+++ player/player.gd
@@ -14,18 +14,29 @@ func _ready():
 	$AnimatedSprite2D.play()
 
 func _process(delta):
+	# If user wants to jump, start the MockAirTimer and change the movement state to airborne
+	if Input.is_action_just_pressed("Jump") and movement_state != MovementState.AIRBORNE:
+		$MockAirTimer.start()
+		movement_state = MovementState.AIRBORNE
+
 	if Input.is_action_pressed("Run right"):
 		orientation = Orientation.RIGHT
-		movement_state = MovementState.RUNNING
+		# Only change movement state to running if not airborne
+		movement_state = MovementState.RUNNING if movement_state != MovementState.AIRBORNE else MovementState.AIRBORNE
 	elif Input.is_action_pressed("Run left"):
 		orientation = Orientation.LEFT
-		movement_state = MovementState.RUNNING
+		# Only change movement state to running if not airborne
+		movement_state = MovementState.RUNNING if movement_state != MovementState.AIRBORNE else MovementState.AIRBORNE
 	else:
-		movement_state = MovementState.IDLE
+		# Only change movement state to idle if not airborne
+		movement_state = MovementState.IDLE    if movement_state != MovementState.AIRBORNE else MovementState.AIRBORNE
 
 	match (movement_state):
 		MovementState.RUNNING:
 			$AnimatedSprite2D.animation = "running"
+		# This was added
+		MovementState.AIRBORNE:
+			$AnimatedSprite2D.animation = "jumping"
 		_: # MovementState.IDLE
 			$AnimatedSprite2D.animation = "idle"
 
@@ -33,3 +44,7 @@ func _process(delta):
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
+
+
+func _on_mock_air_timer_timeout():
+	movement_state = MovementState.IDLE
