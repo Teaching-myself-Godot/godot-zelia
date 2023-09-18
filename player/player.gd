8a5b831edf1a267432272b26759379d129ac28d6
René van der Ark <rene.vanderark@kb.nl>
2023-09-18 20:16:23 +0200

    fixes player as a Character2D

--- player/player.gd
+++ player/player.gd
@@ -1,67 +1,70 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 enum Orientation   { LEFT, RIGHT }
 enum MovementState { IDLE, RUNNING, AIRBORNE }
 
-# We will want to debug these states, let's export them as well
-@export var movement_state : int
-@export var orientation   : int
+# I removed the exports for now, no debugging needed at the moment
+var movement_state : int
+var orientation    : int
 
-var can_detect_landing : bool
+# Get the gravity from the project settings so you can sync with rigid body nodes.
+# NOTE: I changed the default from 980 to 1300, Zelia jumps high yet falls fast.
+var gravity    = ProjectSettings.get_setting("physics/2d/default_gravity")
+# The most realistic speed for Zelia's feet
+var speed      = 120.0
+# Funnily the original game had jump_speed set to -4.0 and gravity to 13.0
+var jump_speed = -400.0
 
+# No changes here
 func _ready():
 	movement_state = MovementState.IDLE
 	orientation    = Orientation.RIGHT
 	$AnimatedSprite2D.play()
-	contact_monitor = true
-	max_contacts_reported = 4
-	gravity_scale = 1.5
-	can_detect_landing = true
 
-
-func _integrate_forces(state):
-	if movement_state == MovementState.AIRBORNE:
-		if can_detect_landing:
-			for x in range(state.get_contact_count()):
-				var ci = state.get_contact_local_normal(x)
-				if ci.dot(Vector2(0, -1)) == 1.0:
-					print("found floor")
-					movement_state = MovementState.IDLE
-				else:
-					set_axis_velocity(Vector2(0, 0))
-					apply_central_impulse(state.get_contact_local_normal(x) * 50)
-					
+# Changed _process to _physics_process
 func _physics_process(delta):
-	# If user wants to jump, start the MockAirTimer and change the movement state to airborne
-	if Input.is_action_just_pressed("Jump") and movement_state != MovementState.AIRBORNE:
-		$JumpSound.play() # the new line
-		movement_state = MovementState.AIRBORNE
-		set_axis_velocity(Vector2(0, -400.0))
-		$MockAirTimer.start()
-		can_detect_landing = false
-	
+	# Apply the gravity.
+	velocity.y += gravity * delta
 
-
+	# Update the MovementState based on the collisions observed
 	if movement_state == MovementState.AIRBORNE:
-		if Input.is_action_pressed("Run right"):
-			orientation = Orientation.RIGHT
-			set_axis_velocity(Vector2(80.0, 0))
-		elif Input.is_action_pressed("Run left"):
-			orientation = Orientation.LEFT
-			set_axis_velocity(Vector2(-80.0, 0))
-	else:
-		if Input.is_action_pressed("Run right"):
-			orientation = Orientation.RIGHT
-			movement_state = MovementState.RUNNING
-			set_axis_velocity(Vector2(120.0, 0.0))
-		elif Input.is_action_pressed("Run left"):
-			orientation = Orientation.LEFT
-			movement_state = MovementState.RUNNING
-			set_axis_velocity(Vector2(-120.0, 0.0))
-		else:
+		# If she's airborne right now
+		if is_on_floor():
+			# .. and hits the floor, she's idle
 			movement_state = MovementState.IDLE
-			set_axis_velocity(Vector2(0.0, 0.0))
+		elif Input.is_action_pressed("Run right"):
+			# Else you can still move her right
+			orientation = Orientation.RIGHT
+			velocity.x = speed
+		elif Input.is_action_pressed("Run left"):
+			# ... and left
+			orientation = Orientation.LEFT
+			velocity.x = -speed
+		# But you cannot let her remove x momentum
+	else:
+		# Else we are not airborne right now
+		if Input.is_action_pressed("Run right"):
+			# so we run right when run right is pressed
+			orientation = Orientation.RIGHT
+			movement_state = MovementState.RUNNING
+			velocity.x = speed
+		elif Input.is_action_pressed("Run left"):
+			# .. and left ...
+			orientation = Orientation.LEFT
+			movement_state = MovementState.RUNNING
+			velocity.x = -speed
+		else:
+			# and stand idle if no x-movement button is pressed
+			velocity.x = 0
+			movement_state = MovementState.IDLE  
 
+	# Handle Jump, only when on the floor
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		$JumpSound.play()
+		movement_state = MovementState.AIRBORNE
+		velocity.y = jump_speed
+	
+	# This code has not changed
 	match (movement_state):
 		MovementState.RUNNING:
 			$AnimatedSprite2D.animation = "running"
@@ -71,12 +74,12 @@ func _physics_process(delta):
 		_: # MovementState.IDLE
 			$AnimatedSprite2D.animation = "idle"
 
+	# Neither had this
 	if orientation == Orientation.LEFT:
 		$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.flip_h = false
 
+	# Yet this is new
+	move_and_slide()
 
-
-func _on_mock_air_timer_timeout():
-	can_detect_landing = true
