fe515bfbf79cc09236f972892dfdcc9e48314d3f
René van der Ark <rene.vanderark@kb.nl>
2023-09-24 18:18:37 +0200

    adds world.gd

---
+++ world.gd
@@ -0,0 +1,10 @@
+extends TileMap
+
+
+
+func _on_player_cast_projectile(spell_class, direction, origin):
+	var spell = spell_class.instantiate()
+	add_child(spell)
+	spell.rotation = direction
+	spell.position = origin
+	spell.velocity = Vector2.from_angle(direction) * 150.0
