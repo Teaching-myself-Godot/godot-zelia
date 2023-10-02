0f2ee97e0e97364e26c9e6391e1cdaa2ad28b60f
René van der Ark <rene.vanderark@kb.nl>
2023-10-02 13:56:25 +0200

    proof

--- projectiles/fireball/fireball.gd
+++ projectiles/fireball/fireball.gd
@@ -5,17 +5,25 @@ extends Area2D
 @export var damage = 2
 var done = false
 
+var dissipation_shader = preload("res://dissipation_shader.gdshader")
+
+
+var dissipate_fade = 0.0
+var dissipate_scatter = 0.0
 func _ready():
-	# Start playing the "default" animation
-	$AnimatedSprite2D.play("default")
+	var material : ShaderMaterial = $Sprite2D.material
+	material.shader = dissipation_shader.duplicate()
 	
-	if "dissipate" not in $AnimatedSprite2D.sprite_frames.get_animation_names():
-		# Add a new animation to the SpriteFrames instance of the $AnimatedSprite2D node
-		$AnimatedSprite2D.sprite_frames.add_animation("dissipate")
-		# Loop through all rendition images in the global singleton fireball_dissipate
-		for rendition in TextureRenditions.fireball_dissipate:
-			# Add them as a frame to 
-			$AnimatedSprite2D.sprite_frames.add_frame("dissipate", rendition)
+#	# Start playing the "default" animation
+#	$AnimatedSprite2D.play("default")
+#
+#	if "dissipate" not in $AnimatedSprite2D.sprite_frames.get_animation_names():
+#		# Add a new animation to the SpriteFrames instance of the $AnimatedSprite2D node
+#		$AnimatedSprite2D.sprite_frames.add_animation("dissipate")
+#		# Loop through all rendition images in the global singleton fireball_dissipate
+#		for rendition in TextureRenditions.fireball_dissipate:
+#			# Add them as a frame to 
+#			$AnimatedSprite2D.sprite_frames.add_frame("dissipate", rendition)
 
 func _physics_process(delta):
 	# Update position by velocity-vector
@@ -27,7 +35,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_body_entered(body):
 	# play the dissipate animation we coded
-	$AnimatedSprite2D.play("dissipate")
+#	$AnimatedSprite2D.play("dissipate")
 	# start the new timer in stead of calling queue_free here
 	$DissipateTimer.start()
 	# slow it down to 1/10th the speed
@@ -36,4 +44,9 @@ func _on_body_entered(body):
 		body.take_damage(damage)
 
 func _on_dissipate_timer_timeout():
-	queue_free()
+	dissipate_fade += 0.1
+	dissipate_scatter += 0.1
+	var material : ShaderMaterial = $Sprite2D.material
+	material.set_shader_parameter("fade", dissipate_fade)
+	material.set_shader_parameter("scatter", dissipate_scatter)
+	
