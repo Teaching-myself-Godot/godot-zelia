19a011e3c3f413346aa76ff9d17adb15419cc5a5
René van der Ark <rene.vanderark@kb.nl>
2023-10-04 22:42:29 +0200

    removes breakable terrain scene in favor of terrain metadata.

--- terrains/breakable_terrains.gd
+++ terrains/breakable_terrains.gd
@@ -10,20 +10,20 @@ signal add_breakable_tile(
 )
 
 func _ready():
-	visible = false
-
-	for tile in get_used_cells(0):
-		var source_id = get_cell_source_id(0, tile)
-		var tileset_source : TileSetAtlasSource = tile_set.get_source(source_id)
-		var tile_atlas_coords = get_cell_atlas_coords(0, tile)
-		var tile_data = tileset_source.get_tile_data(tile_atlas_coords, 0)
-		emit_signal(
-			"add_breakable_tile",
-			tile * 15,
-			tileset_source.texture,
-			tile_atlas_coords,
-			tile_data.get_collision_polygon_points(0, 0),
-			get_meta("hp"),
-			get_meta("falls_down")
-		)
-	queue_free()
+	if get_meta("breakable") or get_meta("falls_down"):
+		visible = false
+		for tile in get_used_cells(0):
+			var source_id = get_cell_source_id(0, tile)
+			var tileset_source : TileSetAtlasSource = tile_set.get_source(source_id)
+			var tile_atlas_coords = get_cell_atlas_coords(0, tile)
+			var tile_data = tileset_source.get_tile_data(tile_atlas_coords, 0)
+			emit_signal(
+				"add_breakable_tile",
+				tile * 15,
+				tileset_source.texture,
+				tile_atlas_coords,
+				tile_data.get_collision_polygon_points(0, 0),
+				get_meta("hp"),
+				get_meta("falls_down")
+			)
+		queue_free()
