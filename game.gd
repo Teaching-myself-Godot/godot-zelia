0d19640841dbfdbdbdb5a4f1e45c034e95e57040
René van der Ark <rene.vanderark@kb.nl>
2023-10-15 17:35:03 +0200

    adds signal, emit and listener for breakable tiles.

--- game.gd
+++ game.gd
@@ -6,3 +6,10 @@ func _on_player_cast_projectile(spell_class, direction, origin):
 	spell.rotation = direction
 	spell.position = origin
 	spell.velocity = Vector2.from_angle(direction) * 150.0
+
+
+func _on_breakable_terrains_add_breakable_tile(target_pos, texture, texture_pos, collisigon):
+	print("Target position of BreakableTile:   " + str(target_pos))
+	print("Atlas texture resource dir:         " + str(texture.resource_path))
+	print("Position of this tile in the atlas: " + str(texture_pos))
+	print("Collision polygon of this tile:     " + str(collisigon))
--- terrains.gd
+++ terrains.gd
@@ -1,16 +1,26 @@
 extends TileMap
 
+signal add_breakable_tile(
+	position     : Vector2i, 
+	texture      : Texture2D, 
+	texture_pos  : Vector2i,
+	# collisigon is my personal shorthand for "collision polygon"
+	collisigon   : PackedVector2Array
+)
+
+
 func _ready():
 	if get_meta("breakable"):
-		print (name + " is breakable")
-		print (name + "'s origin is: " + str(position))
 		# Loop through the tile positions in our current (only) layer
 		for cell in get_used_cells(0):
-			print("Tile grid position:            " + str(cell))
-			print("BreakableTile target position: " + str(Vector2i(position) + cell * tile_set.tile_size))
 			var source_id = get_cell_source_id(0, cell)
 			var tileset_source : TileSetAtlasSource = tile_set.get_source(source_id)
-			print("TileSetAtlasSource:            " + tileset_source.resource_name)
 			var tile_atlas_coords = get_cell_atlas_coords(0, cell)
-			print("tile_atlas_coords:             " + str(tile_atlas_coords * tile_set.tile_size))
-			print("texture:                       " + tileset_source.texture.resource_path)
+			var tile_data = tileset_source.get_tile_data(tile_atlas_coords, 0)
+			emit_signal(
+				"add_breakable_tile",
+				Vector2i(position) + cell * tile_set.tile_size,
+				tileset_source.texture,
+				tile_atlas_coords * tile_set.tile_size,
+				tile_data.get_collision_polygon_points(0, 0)
+			)
