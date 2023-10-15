8df5ab843e445e4b6ef92a6bf743a398f1853a70
René van der Ark <rene.vanderark@kb.nl>
2023-10-15 14:17:57 +0200

    continues the breakable tile loop using poor-man's-debugger.

--- terrains.gd
+++ terrains.gd
@@ -8,3 +8,9 @@ func _ready():
 		for cell in get_used_cells(0):
 			print("Tile grid position:            " + str(cell))
 			print("BreakableTile target position: " + str(Vector2i(position) + cell * tile_set.tile_size))
+			var source_id = get_cell_source_id(0, cell)
+			var tileset_source : TileSetAtlasSource = tile_set.get_source(source_id)
+			print("TileSetAtlasSource:            " + tileset_source.resource_name)
+			var tile_atlas_coords = get_cell_atlas_coords(0, cell)
+			print("tile_atlas_coords:             " + str(tile_atlas_coords * tile_set.tile_size))
+			print("texture:                       " + tileset_source.texture.resource_path)
